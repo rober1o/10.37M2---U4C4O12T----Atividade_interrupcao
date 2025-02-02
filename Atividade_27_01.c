8f8919d2d3aeb661c65041caa87f104b517dfd0f
rober1o <robertoif701@gmail.com>
2025-02-01 22:12:20 -0300

    Correção do pino do LED RGB no arquivo JSON e organização do código para melhor entendimento

--- Atividade_27_01.c
+++ Atividade_27_01.c
@@ -1,52 +1,4 @@
 #include "Atividade_27_01.h"
-#include "numeros.h"
-#include "pico/bootrom.h"
-#include "pico/time.h"  // Para usar timestamps
-
-
-#define DEBOUNCE_TIME 200 // Tempo de debounce em milissegundos
-#define LED_RED 13 
-#define LED_BLUE 12
-#define LED_GREEN 11
-#define BUTTON_A 5
-#define BUTTON_B 6
-#define brilho_padrao 100
-
-volatile uint32_t ultimo_tempoA = 0;
-volatile uint32_t ultimo_tempoB = 0;
-repeating_timer_t timer;
-volatile bool ligado = false;
-int numero = 0; 
-PIO pio;
-int sm;
-
-void gpio_irq_handler(uint gpio, uint32_t events);
-
-
-void iniciar_led() {
-    gpio_init(LED_RED); 
-    gpio_init(LED_BLUE);
-    gpio_init(LED_GREEN);    
-    gpio_set_dir(LED_RED, GPIO_OUT); 
-    gpio_set_dir(LED_BLUE, GPIO_OUT); 
-    gpio_set_dir(LED_GREEN, GPIO_OUT);
-    gpio_set_dir(BUTTON_A, GPIO_IN);
-    gpio_set_dir(BUTTON_B, GPIO_IN);
-    gpio_pull_up(BUTTON_A); // Ativa resistor pull-up interno
-    gpio_pull_up(BUTTON_B);
-
-    
-    // Configura interrupção no botão (borda de descida)
-    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
-    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
-}
-
-// Função de callback para alternar o estado do LED
-bool piscar_led_red_callback(repeating_timer_t *t) {
-    ligado = !ligado;  // Alterna o estado do LED
-    gpio_put(LED_RED, ligado);  // Atualiza o estado do LED
-    return true;  // Retorna true para continuar o temporizador
-}
 
 int main()
 {
@@ -62,7 +14,7 @@ int main()
     sm = pio_claim_unused_sm(pio, true);
     Matriz_5x5_program_init(pio, sm, offset, matriz_pin);
 
-    iniciar_led();
+    iniciar_pinos();
  
     add_repeating_timer_ms(100, piscar_led_red_callback, NULL, &timer);
 
@@ -73,27 +25,69 @@ int main()
 
 }
 
+// Rotina para iniciar e configurar os LEDs e botões
+
+void iniciar_pinos() {
+    gpio_init(LED_RED); 
+    gpio_init(LED_BLUE);
+    gpio_init(LED_GREEN);    
+    gpio_set_dir(LED_RED, GPIO_OUT); 
+    gpio_set_dir(LED_BLUE, GPIO_OUT); 
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_set_dir(BUTTON_B, GPIO_IN);
+    gpio_pull_up(BUTTON_A); 
+    gpio_pull_up(BUTTON_B);
+
+    // Configura interrupção no botão
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
+}
+
+
+// Função de callback para alternar o estado do LED
+bool piscar_led_red_callback(repeating_timer_t *t) {
+    ligado = !ligado;  // Alterna o estado do LED
+    gpio_put(LED_RED, ligado);  // Atualiza o estado do LED
+    return true;  // Retorna true para continuar o temporizador
+}
+
 // Função de interrupção do botão
 void gpio_irq_handler(uint gpio, uint32_t events) {
-    uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
 
-    if (gpio == BUTTON_A && (events & GPIO_IRQ_EDGE_FALL)) { 
-        if (tempo_atual - ultimo_tempoA > DEBOUNCE_TIME) { 
-            if (numero < 9) numero++;  // Incrementa o número
-            exibir_numero();  // Chama a função para exibir o número
-            ultimo_tempoA = tempo_atual;
+    if ((events & GPIO_IRQ_EDGE_FALL) && debounce_ok(gpio)) { // Verifica se passou 200ms desde a última chamada do botão
+
+        if (gpio == BUTTON_A && numero < 9) { // Verifica se a interrupçãoveio do botão A e se já não atingiu o valor máximo da matriz
+            numero++;  // Incrementa o número
+        } else if (gpio == BUTTON_B && numero > 0) { // Verifica se a interrupção veio do botão B e se já não atingiu o valor mínimo da matriz
+            numero--;  // Decrementa o número
         }
-    } else if (gpio == BUTTON_B && (events & GPIO_IRQ_EDGE_FALL)) {
+
+        exibir_numero();  // Atualiza a exibição do número
+    }
+}
+
+// Função para verificar debounce
+bool debounce_ok(uint gpio) {
+    uint32_t tempo_atual = to_ms_since_boot(get_absolute_time()); 
+    
+    if (gpio == BUTTON_A) {
+        if (tempo_atual - ultimo_tempoA > DEBOUNCE_TIME) {
+            ultimo_tempoA = tempo_atual;
+            return true;
+        }
+    } else if (gpio == BUTTON_B) {
         if (tempo_atual - ultimo_tempoB > DEBOUNCE_TIME) {
-            if (numero > 0) numero--;  // Decrementa o número
-            exibir_numero();  // Chama a função para exibir o número
             ultimo_tempoB = tempo_atual;
+            return true;
         }
     }
+    return false;
 }
 
 
 
+// Rotina para exibir os números na matriz de LED
 void exibir_numero() {
     switch (numero) {
         case 0: 
--- Atividade_27_01.h
+++ Atividade_27_01.h
@@ -6,26 +6,39 @@
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "pio_wave.pio.h"
+#include "numeros.h"
+#include "pico/bootrom.h"
+#include "pico/time.h" 
 
 
-
-//número de LEDs
+//Definição dos pinos dos LED e botões, e definição do tempo de debouce e intencidade do brilho.
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13 
+#define BUTTON_A 5
+#define BUTTON_B 6
+#define brilho_padrao 100
+#define DEBOUNCE_TIME 200 
+#define matriz_pin 7
 #define NUM_PIXELS 25
 
-//Pino de Dados para a Matriz
-#define matriz_pin 7
+//Declaração das variáveis globais usadas no arquivo C
+volatile uint32_t ultimo_tempoA = 0;
+volatile uint32_t ultimo_tempoB = 0;
+repeating_timer_t timer;
+volatile bool ligado = false;
+int numero = 0; 
+PIO pio;
+int sm;
 
 
 
-//Criação de Pio
-PIO pio = pio0;
-extern int sm;
-
-
-
-void imprimir_binario(int num);
-uint32_t matrix_rgb(double b, double r, double g);
-void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b);
+// Declaração das funções a serem implementadas no arquivo C
+bool piscar_led_red_callback(repeating_timer_t *t);
+bool debounce_ok(uint gpio);
+void gpio_irq_handler(uint gpio, uint32_t events);
+void exibir_numero(void);
+void iniciar_pinos(void);
 void desenha_fig(uint32_t *_matriz, uint8_t _intensidade, PIO pio, uint sm);
 
 #endif // Atividade_27_01
